bc49c10a598a0f2295137a22c9c99843ec0748d4
Trenton Greer <yoshigreer@gmail.com>
2017-02-09 22:56:37 -0700

    checkpoint 3 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,12 +9,12 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
-    entries.each do |entry|
+    @entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
     end
-    entries.insert(index, Entry.new(name, phone_number, email))
+    @entries.insert(index, Entry.new(name, phone_number, email))
   end
 end
